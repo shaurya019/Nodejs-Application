77a31f684c83ced2f241d9cdb44067a92424694f
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-05-21 12:00:18 +0530

    jobs api

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -3,20 +3,111 @@ import mongoose from "mongoose";
 import moment from "moment";
 
 export const createJobController = async (req, res, next) => {
-    const { company, position } = req.body;
-    if (!company || !position) {
-      next("Please Provide All Fields");
-    }
-    req.body.createdBy = req.user.userId;
-    const job = await jobsModel.create(req.body);
-    res.status(201).json({ job });
+  const { company, position } = req.body;
+  if (!company || !position) {
+    next("Please Provide All Fields");
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await jobsModel.create(req.body);
+  res.status(201).json({ job });
+};
+
+export const getAllJobsController = async (req, res, next) => {
+  const jobs = await jobsModel.find({ createdBy: req.user.userId });
+  res.status(200).json({
+    totalJobs: jobs.length,
+    jobs,
+  });
+};
+
+export const updateJobController = async (req, res, next) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+  //validation
+  if (!company || !position) {
+    next("Please Provide All Fields");
+  }
+  //find job
+  const job = await jobsModel.findOne({ _id: id });
+  //validation
+  if (!job) {
+    next(`no jobs found with this id ${id}`);
+  }
+  if (!req.user.userId === job.createdBy.toString()) {
+    next("Your Not Authorized to update this job");
+    return;
+  }
+  const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  //res
+  res.status(200).json({ updateJob });
+};
+
+// ======= DELETE JOBS ===========
+export const deleteJobController = async (req, res, next) => {
+  const { id } = req.params;
+  //find job
+  const job = await jobsModel.findOne({ _id: id });
+  //validation
+  if (!job) {
+    next(`No Job Found With This ID ${id}`);
+  }
+  if (!req.user.userId === job.createdBy.toString()) {
+    next("Your Not Authorize to delete this job");
+    return;
+  }
+  await job.deleteOne();
+  res.status(200).json({ message: "Success, Job Deleted!" });
+};
+
+
+export const jobStatsController = async (req, res, next) => {
+  const stats = await jobsModel.aggregate([
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user.userId),
+      },
+    },
+  ]);
+  const defaultStats = {
+    pending: stats.pending || 0,
+    reject: stats.reject || 0,
+    interview: stats.interview || 0,
   };
 
-export const getAllJobsController = async (req, res, next) => {};
+  let monthlyApplication = await jobsModel.aggregate([
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user.userId),
+      },
+    },
+    {
+      $group: {
+        _id: {
+          year: { $year: "$createdAt" },
+          month: { $month: "$createdAt" },
+        },
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+  monthlyApplication = monthlyApplication
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+      return { date, count };
+    })
+    .reverse();
 
-export const updateJobController = async (req, res, next) => { }; 
-
-export const deleteJobController = async (req, res, next) => {};
-
-export const jobStatsController = async (req, res, next) => {};
-
+  res.stats(200).json({ totalJobs: stats.length, defaultStats, monthlyApplication });
+};
